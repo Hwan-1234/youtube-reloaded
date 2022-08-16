8e7fde8962c14855d4701c56e518acbd6cfb604f
Hwan-1234 <mwwo88@gmail.com>
2022-08-17 06:25:11 +0900

    user profile

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -200,5 +200,14 @@ export const postChangePassword = async (req, res) => {
   await user.save();
   return res.redirect("/users/logout");
 };
-export const edit = (req, res) => res.send("Edit User");
-export const see = (req, res) => res.send("See User");
+export const see = async (req, res) => {
+  const { id } = req.params;
+  const user = await User.findById(id);
+  if (!user) {
+    return res.status(404).render("404", { pageTitle: "User not found." });
+  }
+  return res.render("users/profile", {
+    pageTitle: user.name,
+    user,
+  });
+};
